9eb7dd2337419f3e38636fe4a02afaef36567658
Jean Frusca <jeang96@gmail.com>
2022-10-19 08:39:14 -0300

    saving changes for later

--- src/app.js
+++ src/app.js
@@ -10,9 +10,23 @@ app.use(express.json());
 dotenv.config();
 
 app.get("/status", (req, res) => {
-  res.sendStatus(201);
+	res.sendStatus(201);
+});
+
+app.get("/users/:id", (req, res) => {
+	try {
+		const { id } = req.params;
+		if (!id) {
+			return res.sendStatus(400);
+		}
+
+		return res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server listen on port ${process.env.PORT}`);
+	console.log(`Server listen on port ${process.env.PORT}`);
 });
