b43550039b51f1fb98784a20f1297a8482bb7a73
Jean Frusca <jeang96@gmail.com>
2022-10-20 18:04:14 -0300

    refactoring and creating a repository for the connection with databse

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,5 +1,8 @@
-import connection from "../database/database.js";
-import { listUsers } from "../repositories/users.repositories.js";
+import {
+	listUserPosts,
+	listUsers,
+	listUsersById,
+} from "../repositories/users.repositories.js";
 import {
 	badRequestResponse,
 	serverErrorResponse,
@@ -12,28 +15,13 @@ async function getUserInfo(req, res) {
 		if (!id) {
 			badRequestResponse(res);
 		}
-		const userExists = await connection.query(
-			`
-      SELECT * FROM users WHERE id = $1;
-    `,
-			[id]
-		);
+		const userExists = await listUsersById(id);
 		if (userExists.rowCount === 0) {
 			return res.sendStatus(404);
 		}
 
-		const userInfo = await connection.query(
-			`
-        SELECT 
-            users.id, users.username, users.pictureurl,
-            json_agg(posts.*) AS posts
-        FROM users
-        JOIN posts ON users.id = posts.userid
-        WHERE users.id = $1
-        GROUP BY (users.id)
-        ;`,
-			[id]
-		);
+		const userInfo = await listUserPosts(id);
+
 		return res.status(STATUS_CODE.OK).send(userInfo.rows[0]);
 	} catch (error) {
 		serverErrorResponse(res, error);
--- src/repositories/users.repositories.js
+++ src/repositories/users.repositories.js
@@ -1,4 +1,14 @@
-import connection from "../database/database";
+import connection from "../database/database.js";
+
+async function listUsersById(id) {
+	const result = await connection.query(
+		`
+        SELECT * FROM users WHERE id = $1;
+    `,
+		[id]
+	);
+	return result;
+}
 
 async function listUsers(text) {
 	const result = await connection.query(
@@ -10,4 +20,22 @@ async function listUsers(text) {
 	return result;
 }
 
-export { listUsers };
+async function listUserPosts(id) {
+	const result = await connection.query(
+		`
+        SELECT 
+            users.id, 
+            users.username, 
+            users.pictureurl,
+            json_agg(posts.*) AS posts
+        FROM users
+        JOIN posts ON users.id = posts.userid
+        WHERE users.id = $1
+        GROUP BY (users.id)
+        ;`,
+		[id]
+	);
+	return result;
+}
+
+export { listUsers, listUserPosts, listUsersById };
