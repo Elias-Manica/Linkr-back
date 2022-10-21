09f3ab0d2933d6ba983da5d9060e1929c04e3096
elias <eliasmanica@poli.ufrj.br>
2022-10-20 21:44:41 -0300

    bring userid in querys

--- src/repositories/posts.repositories.js
+++ src/repositories/posts.repositories.js
@@ -2,7 +2,7 @@ import connection from "../database/database.js";
 
 async function listPosts() {
   const response = await connection.query(
-    `SELECT P1.id, P1.link, P1.text, P1.date, U1.username, U1.pictureurl, COUNT( DISTINCT U2.username) AS "qtdlikes", json_agg(DISTINCT U2.username) AS "nameusersliked", json_agg( DISTINCT h2.name) AS "hashtags" FROM posts P1 JOIN users U1 ON P1.userid = U1.id LEFT JOIN likes L1 ON P1.id = L1.postid LEFT JOIN users U2 ON U2.id = L1.userid LEFT JOIN posthashtags H1 ON H1.postid = P1.id LEFT JOIN hashtags H2 ON h2.id = h1.hashtagid GROUP BY P1.id, U1.username, U1.pictureurl ORDER BY P1.id DESC LIMIT 20;`
+    `SELECT P1.id, P1.link, P1.text, P1.date, U1.id AS "userid", U1.username, U1.pictureurl, COUNT( DISTINCT U2.username) AS "qtdlikes", json_agg(DISTINCT U2.username) AS "nameusersliked", json_agg( DISTINCT h2.name) AS "hashtags" FROM posts P1 JOIN users U1 ON P1.userid = U1.id LEFT JOIN likes L1 ON P1.id = L1.postid LEFT JOIN users U2 ON U2.id = L1.userid LEFT JOIN posthashtags H1 ON H1.postid = P1.id LEFT JOIN hashtags H2 ON h2.id = h1.hashtagid GROUP BY P1.id, U1.username, U1.pictureurl, U1.id ORDER BY P1.id DESC LIMIT 20;`
   );
   return response;
 }
@@ -17,7 +17,7 @@ async function listHashtags() {
 
 async function listPostsBasedOnNameHashtag(name) {
   const response = await connection.query(
-    `SELECT P1.id, P1.link, P1.text, P1.date, U1.username, U1.pictureurl, COUNT( DISTINCT U2.username) AS "qtdlikes", json_agg(DISTINCT U2.username) AS "nameusersliked" FROM posts P1 JOIN users U1 ON P1.userid = U1.id LEFT JOIN likes L1 ON P1.id = L1.postid LEFT JOIN users U2 ON U2.id = L1.userid LEFT JOIN posthashtags H1 ON H1.postid = P1.id LEFT JOIN hashtags H2 ON h2.id = h1.hashtagid WHERE h2.name = $1 GROUP BY P1.id, U1.username, U1.pictureurl  ORDER BY P1.id DESC LIMIT 20 ;`,
+    `SELECT P1.id, P1.link, P1.text, P1.date, U1.id AS "userid", U1.username, U1.pictureurl, COUNT( DISTINCT U2.username) AS "qtdlikes", json_agg(DISTINCT U2.username) AS "nameusersliked" FROM posts P1 JOIN users U1 ON P1.userid = U1.id LEFT JOIN likes L1 ON P1.id = L1.postid LEFT JOIN users U2 ON U2.id = L1.userid LEFT JOIN posthashtags H1 ON H1.postid = P1.id LEFT JOIN hashtags H2 ON h2.id = h1.hashtagid WHERE h2.name = $1 GROUP BY P1.id, U1.username, U1.pictureurl, U1.id  ORDER BY P1.id DESC LIMIT 20 ;`,
     [name]
   );
 
