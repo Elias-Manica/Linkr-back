8da921d3926fe37a75f1ae162443b352d5adaafc
Jean Frusca <jeang96@gmail.com>
2022-10-24 13:01:06 -0300

    preparing some functions to implement like

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,50 +1,55 @@
-import { deleteSession, insertSession, insertUser } from "../repositories/auth.repositories.js";
-import { createdResponse, okResponse, serverErrorResponse } from "./helper.controllers.js";
-import uuid from "uuid";
+import {
+	deleteSession,
+	insertSession,
+	insertUser,
+} from "../repositories/auth.repositories.js";
+import {
+	createdResponse,
+	okResponse,
+	serverErrorResponse,
+} from "./helper.controllers.js";
+import { v4 as uuid } from "uuid";
 import bcrypt from "bcrypt";
 
 async function createUser(req, res) {
-    const { email, password, username, pictureurl } = res.locals.body;
+	const { email, password, username, pictureurl } = res.locals.body;
 
-    const passwordHash = bcrypt.hashSync(password, 10);
+	const passwordHash = bcrypt.hashSync(password, 10);
 
-    try {
-        await insertUser(email, passwordHash, username, pictureurl);
+	try {
+		await insertUser(email, passwordHash, username, pictureurl);
 
-        return createdResponse(res);
-
-    } catch(error) {
-        return serverErrorResponse(res, error);
-    }
+		return createdResponse(res);
+	} catch (error) {
+		return serverErrorResponse(res, error);
+	}
 }
 
 async function createLogin(req, res) {
-    const { id, username, pictureurl } = res.locals.user;
-    const userid = id;
-    const token = uuid();
+	const { id, username, pictureurl } = res.locals.user;
+	const userid = id;
+	const token = uuid();
 
-    try {
-        await deleteSession(userid);
-        await insertSession(token, userid);
+	try {
+		await deleteSession(userid);
+		await insertSession(token, userid);
 
-        return okResponse(res, { username, pictureurl, token });
-        
-    } catch(error) {
-        return serverErrorResponse(res, error);
-    }
+		return okResponse(res, { userid, username, pictureurl, token });
+	} catch (error) {
+		return serverErrorResponse(res, error);
+	}
 }
 
 async function logout(req, res) {
-    const { userid } = res.locals.user;
+	const { userid } = res.locals.user;
 
-    try {
-        await deleteSession(userid);
+	try {
+		await deleteSession(userid);
 
-        return okResponse(res);
-
-    } catch(error) {
-        return serverErrorResponse(res, error);
-    }
+		return okResponse(res);
+	} catch (error) {
+		return serverErrorResponse(res, error);
+	}
 }
 
 export { createUser, createLogin, logout };
