6895680016e84fd58de84056d0c2b0ca815f3d03
Jean Frusca <jeang96@gmail.com>
2022-10-25 15:51:20 -0300

    feat: server receives a comment and inserts into the database.

--- src/middlewares/post.middleware.js
+++ src/middlewares/post.middleware.js
@@ -1,57 +1,69 @@
-import { serverErrorResponse } from "../controllers/helper.controllers.js";
+import {
+	badRequestResponse,
+	serverErrorResponse,
+} from "../controllers/helper.controllers.js";
 
 import { listPostBasedOnId } from "../repositories/posts.repositories.js";
 
 async function isAvaiableToDelete(req, res, next) {
-  const user = res.locals.user;
-  const { id } = req.params;
-  try {
-    const response = await listPostBasedOnId(id);
+	const user = res.locals.user;
+	const { id } = req.params;
+	try {
+		const response = await listPostBasedOnId(id);
 
-    if (response.rows.length === 0) {
-      res.status(404).send({ message: "O post não foi encontrado" });
-      return;
-    }
+		if (response.rows.length === 0) {
+			res.status(404).send({ message: "O post não foi encontrado" });
+			return;
+		}
 
-    if (user.userid !== response.rows[0].userid) {
-      res
-        .status(401)
-        .send({ message: "Você não tem permissão para excluir esse post!" });
-      return;
-    }
+		if (user.userid !== response.rows[0].userid) {
+			res
+				.status(401)
+				.send({ message: "Você não tem permissão para excluir esse post!" });
+			return;
+		}
 
-    res.locals.postid = response.rows[0].id;
+		res.locals.postid = response.rows[0].id;
 
-    next();
-  } catch (error) {
-    return serverErrorResponse(res, error);
-  }
+		next();
+	} catch (error) {
+		return serverErrorResponse(res, error);
+	}
 }
 
 async function isAvaiableToEdit(req, res, next) {
-  const user = res.locals.user;
-  const { id } = req.params;
-  try {
-    const response = await listPostBasedOnId(id);
+	const user = res.locals.user;
+	const { id } = req.params;
+	try {
+		const response = await listPostBasedOnId(id);
 
-    if (response.rows.length === 0) {
-      res.status(404).send({ message: "O post não foi encontrado" });
-      return;
-    }
+		if (response.rows.length === 0) {
+			res.status(404).send({ message: "O post não foi encontrado" });
+			return;
+		}
 
-    if (user.userid !== response.rows[0].userid) {
-      res
-        .status(401)
-        .send({ message: "Você não tem permissão para editar esse post!" });
-      return;
-    }
+		if (user.userid !== response.rows[0].userid) {
+			res
+				.status(401)
+				.send({ message: "Você não tem permissão para editar esse post!" });
+			return;
+		}
 
-    res.locals.post = response.rows[0];
+		res.locals.post = response.rows[0];
 
-    next();
-  } catch (error) {
-    return serverErrorResponse(res, error);
-  }
+		next();
+	} catch (error) {
+		return serverErrorResponse(res, error);
+	}
 }
 
-export { isAvaiableToDelete, isAvaiableToEdit };
+function hasDescription(req, res, next) {
+	const { description } = req.body;
+
+	if (!description) {
+		return badRequestResponse(res, "description was not sent");
+	}
+	next();
+}
+
+export { isAvaiableToDelete, isAvaiableToEdit, hasDescription };
--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -1,20 +1,22 @@
 import express from "express";
 
 import {
-  listTimeline,
-  listHashtagsFunction,
-  listPostsBasedOnHashtag,
-  deletePost,
-  editPostFunction,
-  likeAPost,
-  dislikeAPost,
+	listTimeline,
+	listHashtagsFunction,
+	listPostsBasedOnHashtag,
+	deletePost,
+	editPostFunction,
+	likeAPost,
+	dislikeAPost,
+	commentAPost,
 } from "../controllers/posts.controller.js";
 
 import { checkAuthorization } from "../middlewares/auth.middlewares.js";
 
 import {
-  isAvaiableToDelete,
-  isAvaiableToEdit,
+	hasDescription,
+	isAvaiableToDelete,
+	isAvaiableToEdit,
 } from "../middlewares/post.middleware.js";
 import validatePostSchema from "../middlewares/postMiddleware.js";
 
@@ -27,10 +29,16 @@ router.delete("/post/:id", checkAuthorization, isAvaiableToDelete, deletePost);
 router.post("/post/like/:postId", likeAPost);
 router.delete("/post/dislike/:postId", dislikeAPost);
 router.put(
-  "/post/:id",
-  checkAuthorization,
-  isAvaiableToEdit,
-  validatePostSchema,
-  editPostFunction
+	"/post/:id",
+	checkAuthorization,
+	isAvaiableToEdit,
+	validatePostSchema,
+	editPostFunction
+);
+router.post(
+	"/post/comment/:postId",
+	checkAuthorization,
+	hasDescription,
+	commentAPost
 );
 export default router;
