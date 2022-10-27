3a051dadafd6682ebad10e1c4af113b8d483e9b8
Jean Frusca <jeang96@gmail.com>
2022-10-27 15:28:51 -0300

    backend return the ids of those users that you are following

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,8 +1,11 @@
 import express from "express";
+import { showFollowing } from "../controllers/follow.controller.js";
 import { getUserPosts, searchUsers } from "../controllers/users.controller.js";
+import { checkAuthorization } from "../middlewares/auth.middlewares.js";
 
 const usersRouter = express.Router();
 usersRouter.get("/users/:id", getUserPosts);
 usersRouter.post("/users", searchUsers);
+usersRouter.get("/users/get/following", checkAuthorization, showFollowing);
 
 export default usersRouter;
